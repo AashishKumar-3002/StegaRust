957198537dd97b075fc3bb1150c0eecc0bdc4499
AashishKumar-3002 <aashish201810kumar@gmail.com>
2024-09-29 00:43:55 +0530

    ui implemented and integration done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-use rocket::{get, post, routes, launch};
-use rocket::serde::{json::Json, Deserialize};
+use std::io::{ErrorKind, Read, Write};
+use std::path::Path;
+use rocket::data::ToByteUnit;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::fs::NamedFile;
+use std::fs::{self, File};
+use rocket::{data, get, launch, options, post, routes, Request, Response};
+use rocket::serde::{json::Json, Deserialize , Serialize};
 use rocket::response::status::Custom;
-use rocket::http::Status;
+use rocket::http::{ContentType, Header, Status};
+use rocket_multipart_form_data::{MultipartFormData, MultipartFormDataField, MultipartFormDataOptions};
+
 
 mod args;
 mod chunk;
@@ -13,6 +21,12 @@ mod png;
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
+#[derive(Serialize)]
+struct StatusResponse {
+    encoding: &'static str,
+    decoding: &'static str,
+    metadata: &'static str,
+}
 
 #[derive(Deserialize, Debug)]
 struct EncodeData {
@@ -43,6 +57,13 @@ struct PrintResponse {
     status: String,
     chunks: Option<Vec<String>>,
     message: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct UploadResponse {
+    status: String,
+    image_path: String,
+    message: String,
 }
 
 // Root endpoint: /
@@ -171,26 +192,188 @@ fn remove_endpoint(remove_data: Json<RemoveData>) -> Custom<Json<PrintResponse>>
                 chunks: None,
                 message: e.to_string(),
             };
-            Custom(Status::BadRequest, Json(response))
+            Custom(Status::Ok, Json(response))
         }
     }
 }
 
+#[post("/upload", data = "<data>")]
+async fn upload(content_type: &ContentType , data: data::Data<'_>) -> Custom<Json<UploadResponse>> {
+
+    if !content_type.is_form_data() {
+        let response = UploadResponse {
+            status: "error".to_string(),
+            image_path: String::new(),
+            message: format!("Not a valid format , ensure it's form data format"),
+        };
+        return Custom(Status::BadRequest, Json(response));
+    }
+    
+    let options = MultipartFormDataOptions::with_multipart_form_data_fields(vec![
+        MultipartFormDataField::file("image")
+            .size_limit(u64::from(32.mebibytes()))
+    ]);
+
+    let multi_form_data = MultipartFormData::parse(content_type, data, options).await.unwrap();
+
+    let file = multi_form_data.files.get("image");
+
+    if let Some(file_fields) = file {
+        let file_field = &file_fields[0];
+
+        let filename = &file_field.file_name;
+        let content_type = &file_field.content_type;
+
+        println!("FileName: {:?}", filename);
+        println!("Content_type: {:?}", content_type);
+
+        // Define the directory path where images will be stored
+        let images_dir = Path::new("./images");
+
+        // Check if the directory exists, if not, create it
+        if !images_dir.exists() {
+            match fs::create_dir_all(images_dir) {
+                Ok(_) => println!("Created images directory"),
+                Err(e) => {
+                    let response = UploadResponse {
+                        status: "error".to_string(),
+                        image_path: String::new(),
+                        message: format!("Failed to create images directory: {}", e),
+                    };
+                    return Custom(Status::InternalServerError, Json(response));
+                }
+            }
+        }
+
+        // Construct the full file path where the image will be saved
+        let file_path = images_dir.join(filename.as_ref().unwrap());
+
+        // Open or create the destination file
+        let mut file = match std::fs::File::create(&file_path) {
+            Ok(f) => f,
+            Err(e) => {
+                let response = UploadResponse {
+                    status: "error".to_string(),
+                    image_path: String::new(),
+                    message: format!("Failed to create file: {}", e),
+                };
+                return Custom(Status::InternalServerError, Json(response));
+            }
+        };
+
+        let path = &file_field.path;
+
+        let mut temp_file = match File::open(path) {
+            Ok(f) => f,
+            Err(e) => {
+                let response = UploadResponse {
+                    status: "error".to_string(),
+                    image_path: String::new(),
+                    message: format!("Failed to open temp file: {}", e),
+                };
+                return Custom(Status::InternalServerError, Json(response));
+            }
+        };
+
+        let mut buffer = Vec::new();
+
+        if let Err(e) = temp_file.read_to_end(&mut buffer) {
+            let response = UploadResponse {
+                status: "error".to_string(),
+                image_path: String::new(),
+                message: format!("Failed to read temp file: {}", e),
+            };
+            return Custom(Status::InternalServerError, Json(response));
+        }
+
+        if let Err(e) = file.write_all(&buffer) {
+            let response = UploadResponse {
+                status: "error".to_string(),
+                image_path: String::new(),
+                message: format!("Failed to write to file: {}", e),
+            };
+            return Custom(Status::InternalServerError, Json(response));
+        }
+
+        // Return success response with the image path
+        let response = UploadResponse {
+            status: "success".to_string(),
+            image_path: file_path.to_string_lossy().to_string(),
+            message: "File processed successfully.".to_string(),
+        };
+        return Custom(Status::Ok, Json(response));
+    }
+
+    let response = UploadResponse {
+        status: "error".to_string(),
+        image_path: String::new(),
+        message: format!("Upload Failed"),
+    };
+    return Custom(Status::BadRequest, Json(response));
+}
+
+
+
 // Status endpoint: /status
 #[get("/status")]
-fn status() -> Json<&'static str> {
-    Json("API is running")
+fn status() -> Json<StatusResponse> {
+    Json(StatusResponse {
+        encoding: "online",
+        decoding: "online",
+        metadata: "online",
+    })
+}
+
+#[get("/download/<file_name>")]
+async fn download_file(file_name: String) -> Option<NamedFile> {
+    let file_path = format!("./images/{}", file_name);
+    NamedFile::open(Path::new(&file_path)).await.ok()
+}
+
+
+pub struct Cors;
+
+#[rocket::async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Fairing",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "http://localhost:3000"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
+        ));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Headers",
+            "Authorization, Content-Type, Accept",
+        ));
+    }
+}
+
+// Handle preflight requests
+#[options("/<_..>")]
+fn all_options() -> &'static str {
+    ""
 }
 
 // Launch the Rocket server
 #[launch]
 pub fn rocket() -> _ {
+
     rocket::build().mount("/", routes![
         index, 
         encode_endpoint, 
         decode_endpoint, 
         print_endpoint, 
         remove_endpoint, 
-        status
-    ])
+        upload,
+        download_file,
+        status,
+        all_options
+    ]).attach(Cors)
 }
